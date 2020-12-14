0f3938a045c9cdd23343de437d32a121aed7f0b7
beletate <vinicius_beletate@hotmail.com>
2020-12-14 18:37:35 -0300

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import 'firebase/database'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBOI2yeEo2BDl0wqIYS3db0tg7nLsk58S8",
+    authDomain: "comentaki-development.firebaseapp.com",
+    projectId: "comentaki-development",
+    storageBucket: "comentaki-development.appspot.com",
+    messagingSenderId: "690777483473",
+    appId: "1:690777483473:web:5b95f9629f356517e13df0"
+};
+
+// Inicializa e exporta a instância
+
+firebase.initializeApp(firebaseConfig)
+
+export default firebase
